8e0f1aee15907279d6394697e0707c87f0e113f8
mchmielczyk <61965599+mchmielczyk@users.noreply.github.com>
2023-07-21 01:11:30 +0200

    cleanup

--- test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/WarGame/test
-# Build directory: D:/WarGame/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
